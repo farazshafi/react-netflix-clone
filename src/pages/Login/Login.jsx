69b06b9459a83327d50c8fc4939824da20d77cbf
Faraz shafi <farazpachu777@gmail.com>
2024-09-24 07:18:13 +0530

    created a static login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,45 @@
-import "./Login.css"
+import { useState } from "react";
+import logo from "../../assets/logo.png";
+import "./Login.css";
 const Login = () => {
-  return (
-    <div className='login'>Login</div>
-  )
-}
+  const [sign, setSign] = useState("Sign In");
 
-export default Login
+  return (
+    <div className="login">
+      <img src={logo} alt="" className="login-logo" />
+      <div className="login-form">
+        <h1>{sign}</h1>
+        <form>
+          {sign === "Sign Up" ? (
+            <input type="text" placeholder="Your name" />
+          ) : (
+            <></>
+          )}
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button>{sign}</button>
+          <div className="form-help">
+            <div className="remember">
+              <input type="checkbox" />
+              <label htmlFor="">Remeber Me</label>
+            </div>
+            <p>Need Help?</p>
+          </div>
+        </form>
+        <div className="form-switch">
+          {sign === "Sign In" ? (
+            <p>
+              New to Netflix ? <span onClick={()=>setSign("Sign Up")}>Sign Up Now</span>
+            </p>
+          ) : (
+            <p>
+              Already have account ? <span onClick={()=>setSign("Sign In")}>Sign In Now</span>
+            </p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
